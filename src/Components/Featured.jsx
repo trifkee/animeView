1b3ccd8ae4ba39eaeca7046aa297bdbadeba216b
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-02 00:01:30 +0100

    Update Featured.jsx

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -2,31 +2,49 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import loading from '../assets/loading.gif'
 
 function Featured() {
+    let randAnime
 
-    const {isError, data} = useQuery('featured-anime', () => {
-        return axios.get(`https://api.jikan.moe/v4/random/anime`)
+    const {isFetching, isLoading, isFetched, isError, data}  = useQuery('featured-anime', () => {
+        return axios.get(`https://api.jikan.moe/v4/top/anime`)
     })
+
+    if(isFetching || isLoading){
+        return  (
+            <div className='loading-gif'>
+                    <img src={loading} alt="loading gif" />
+            </div>
+        )
+    }
 
     if(isError){
         return <h2>Sorry, we were unable to show data.</h2>
     }
 
+    if(isFetched){
+        const animeList = data?.data.data
+
+        const randNum = Math.floor(Math.random() * animeList.length)
+
+        randAnime = animeList[randNum]
+    }
+
     const style = {
-        backgroundImage: `linear-gradient(0deg, #dfdfdf 10%, rgba(0,0,0,0)), url(${data?.data.data.images.jpg.large_image_url})`
+        backgroundImage: `linear-gradient(0deg, #dfdfdf 10%, rgba(0,0,0,0)), url(${randAnime?.images.webp.large_image_url})`
     }
 
   return (
     <>
     <section className='featured-section' style={style}>
         <div className="featured-info">
-            <h2>{data?.data.data.title_english || data?.data.data.titles[1].title}</h2>
-            <p>{data?.data.data.synopsis}</p>
+            <h2>{randAnime?.title_english || randAnime?.titles[1].title}</h2>
+            <p>{randAnime?.synopsis}</p>
         </div>
         <div className="featured-cta">
-            <Link to={`/anime/${data?.data.data.mal_id}`} className="featured-watch shadow">Read more <ion-icon name="eye-sharp"></ion-icon></Link>
-            {data?.data.data.trailer.url ? <a href={data?.data.data.trailer.url} target="_blank" className="featured-trailer shadow">watch trailer <ion-icon name="videocam-sharp"></ion-icon></a> : null}
+            <Link to={`/anime/${randAnime?.mal_id}`} className="featured-watch shadow">Read more <ion-icon name="eye-sharp"></ion-icon></Link>
+            {randAnime?.trailer.url ? <a href={randAnime?.trailer.url} target="_blank" className="featured-trailer shadow">watch trailer <ion-icon name="videocam-sharp"></ion-icon></a> : null}
         </div>
     </section>
     </>
