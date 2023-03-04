b076f7e50070753f42ff7cf676ca38ce6d55d602
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-04 19:59:13 +0100

    Update Popular.jsx

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,30 +1,15 @@
 import React from 'react'
 import Anime from './Anime'
-import { useQuery } from 'react-query'
-import axios from 'axios'
-import loading from '../assets/loading.gif'
 
-function Popular() {
-  const {isFetching, data}  = useQuery('popular-animes', () => {
-    return axios.get(`https://api.jikan.moe/v4/top/anime?limit=18`)
-  })
-
-  if(isFetching){
-    return (
-      <div className='loading-gif'>
-        <p>Loading.</p>
-        <img src={loading} alt="loading gif" />
-      </div>
-    )
-  }
+function Popular({title, genreList}) {  
 
   return (
     <>
     <section className='popular-section'>
       
-            <h2>MOST POPULAR</h2>
+            <h2>{title}</h2>
             <div className="popular-catalog">
-                {data?.data.data.map(anime => <Anime key={anime.mal_id} id={anime.mal_id} image={anime.images.jpg.image_url} title={anime.title_english || anime.titles[2].title} year={anime.year} props={anime}/>)}
+                {genreList?.data.data.map(anime => <Anime key={anime.mal_id} id={anime.mal_id} image={anime.images.jpg.image_url} title={anime.title_english || anime.title_japanese || 'unknown' } year={anime.year} props={anime}/>)}
             </div>
             
     </section>
