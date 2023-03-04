4ecf5bc784be4bbf3109470691a3d06f33d2d06e
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-04 19:58:51 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
 import Featured from '../Components/Featured'
 import Popular from '../Components/Popular'
 import banner from '../assets/banner.png'
 
 function Home() {
+  const {isFetching, data}  = useQuery('popular-animes', () => {
+    return axios.get(`https://api.jikan.moe/v4/top/anime?limit=18`)
+  })
+
   return (
     <>
         <Featured />
-        <Popular />
+        <Popular title='Most popular' genreList={data}/>
         <div className="banner">
           <img src={banner} alt="explore-banner" />
         </div>
