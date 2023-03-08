0217fde5d09edc1218ebddb119d9d4243142d5d2
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-08 13:50:07 +0100

    Updated Favorite
    User can save favorite Anime

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import Anime from '../Components/Anime'
 import AnimeContext from '../store/AnimeContext'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 import loading from '../assets/loading.gif'
 import { useQueries } from 'react-query'
 
@@ -21,31 +22,39 @@ function Favorites() {
         })
     )
 
+    console.log(result.length)
+
     return (
         <section className='popular-section'>
                 
             <div style={{margin:'5rem 0 1rem 0', display:'flex', flexDirection:'column'}} className="heading">
                 <h2 style={{marginBottom:'0', lineHeight:'.55'}}>Favorites <br /><span style={{fontSize:'1rem', fontWeight:'400'}}>Collection of liked Anime</span></h2>
             </div>
-            <div className="popular-catalog">
-                {result.map(anime => {
-                    if(anime?.isFetched){
-                        console.log(anime.data.data.data)
-                        return <Anime key={anime.data.data.data.mal_id} id={anime.data.data.data.mal_id} image={anime.data.data.data.images.jpg.image_url} title={anime.data.data.data.title_english || anime.data.data.data.title_japanese || 'unknown' } year={anime.data.data.data.year} props={anime} />
-                    } else {
-                        return(
-                            <div className='loading-gif'>
-                                <p>Loading.</p>
-                                <img src={loading} alt="loading gif" />
-                            </div>    
-                        )
-                    }
-                })}
-            </div>
-            <div className="popular-catalog">
-                
-            </div>
-            
+            { result.length ? 
+                ( 
+                    <div className="popular-catalog">
+                        {result.map(anime => {
+                            if(anime?.isFetched){
+                                return <Anime key={anime.data.data.data.mal_id} id={anime.data.data.data.mal_id} image={anime.data.data.data.images.jpg.image_url} title={anime.data.data.data.title_english || anime.data.data.data.title_japanese || 'unknown' } year={anime.data.data.data.year} props={anime} />
+                            } else {
+                                return(
+                                    <div className='loading-gif'>
+                                        <p>Loading.</p>
+                                        <img src={loading} alt="loading gif" />
+                                    </div>    
+                                )
+                            }
+                        })}
+                    </div>
+                ) : 
+                (
+                    <section className='searched-anime-404' style={{height:'70vh'}}>
+                        <h2 style={{fontSize:'2rem'}}>Oops! Looks like your favorite list is empty.</h2>
+                        <p style={{fontSize:'1rem'}}>Add your favorite anime to the list</p> 
+                        <Link to='/' className='searched-go-back shadow' style={{display:'flex', alignItems:'center', gap:'1rem',}}><ion-icon name="home-sharp"></ion-icon> home page</Link>
+                    </section>
+                )
+            }
         </section>
     )
 }
