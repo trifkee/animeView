debaf8aa16a97190c64db172d151c726820520ea
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-08 01:18:38 +0100

    Add files via upload

---
+++ src/Pages/Favorites.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useState, useEffect } from 'react'
+import AnimeContext from '../store/AnimeContext'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+function Favorites() {
+    const { favorites } = useContext(AnimeContext)
+
+    const [animeList, setAnimeList] = useState({})
+
+    console.log(favorites)
+    // favorites.forEach( fav => {
+    //     const {data} = useQuery('favAnime', () => {
+    //         return axios (`https://api.jikan.moe/v4/anime/${fav}`)
+    //     })
+
+    //     setAnimeList(prevList => {prevList, data})
+    // })
+
+    // console.log(animeList)
+
+    return (
+        <section className='popular-section'>
+                
+            <h2>Favorites</h2>
+            <div className="popular-catalog">
+                { favorites.map(anime => <p>{anime}</p>) }
+                {/* {genreList?.data.data.map(anime => <Anime key={anime.mal_id} id={anime.mal_id} image={anime.images.jpg.image_url} title={anime.title_english || anime.title_japanese || 'unknown' } year={anime.year} props={anime}/>)} */}
+            </div>
+            
+        </section>
+    )
+}
+
+export default Favorites
