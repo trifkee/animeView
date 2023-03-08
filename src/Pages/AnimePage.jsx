a7163b1c9df7ca514e3d83730e296a729a4cde27
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-08 01:17:24 +0100

    Added 'favorites'

--- src/Pages/AnimePage.jsx
+++ src/Pages/AnimePage.jsx
@@ -1,22 +1,30 @@
 import loading from '../assets/loading.gif'
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Anime from '../Components/Anime'
 import axios from 'axios'
+import AnimeContext from '../store/AnimeContext'
 
 function AnimePage() {
-    
+
+    // GET ANIME ID TROUGHT PARAMS ( URL )
     let { animeId:id } = useParams()
 
-    const {isFetching, isLoading, data:anime, refetch:refetchAnime} = useQuery('anime', () => {
+    // USING CONTEXT
+    const { favorites, setFavorites } = useContext(AnimeContext)
+
+    // GET ANIME BY ID
+    const {isFetching, isLoading, data, refetch:refetchAnime} = useQuery('anime', () => {
         return axios.get(`https://api.jikan.moe/v4/anime/${id}/full`)
     })
 
+    // RECOMMENDED ANIMES BASED ON PARSED ID 
     const {data:recommended, refetch:refetchRecommended} = useQuery('recommended', () => {
         return axios.get(`https://api.jikan.moe/v4/anime/${id}/recommendations`)
     })
 
+    // REFETCH DATA FOR ANIME ON CHANGE (id)
     useEffect(()=>{
         refetchAnime()
         refetchRecommended()
@@ -25,6 +33,7 @@ function AnimePage() {
         }, 500)
     }, [id])
 
+    // IF DATA IS FETCHING OR LOADING, SET LOADING SCREEN
     if(isFetching || isLoading){
         return  (
             <div className='loading-gif'>
@@ -33,51 +42,64 @@ function AnimePage() {
         )
     }
 
-    if(anime.data.data === undefined){
-        return(
-            <section className='searched-anime-404'>
-                <h2>Sorry. 🤕</h2>
-                <p>We could not find data for this Anime</p>
-                <code>Status code: {anime?.data.status}</code>
-                <Link to='/' className='searched-go-back shadow' style={{display:'flex', alignItems:'center', gap:'1rem',}}><ion-icon name="home-sharp"></ion-icon> home page</Link>
-            </section>
-        )
+    // INITIALIZATION
+    let anime = data?.data.data
+    
+    let duration = anime?.duration && anime?.duration.split(' ')[0]
+    let rating = anime?.rating && anime?.rating.split(' ')[0]
+    let score = anime?.score && anime?.score.toFixed(1)
+    let url = anime?.streaming
+    
+    
+    // HANDLE FAVORITE ( REMOVE OR ADD TO ARRAY ID OF ANIME )
+    let isFavorite = favorites.some(el => el === anime?.mal_id)
+
+    const handleFavorite = (e, id) => {
+    
+        if(!isFavorite){
+            return setFavorites(prevFav => [...prevFav, id])
+        } 
+    
+        const filtered = favorites.filter(el => el !== id)
+        setFavorites(filtered)   
     }
-  
-    let duration = anime?.data.data.duration && anime?.data.data.duration.split(' ')[0]
-    let rating = anime?.data.data.rating && anime?.data.data.rating.split(' ')[0]
-    let score = anime?.data.data.score && anime?.data.data.score.toFixed(1)
-    let url = anime?.data.data.streaming
 
-    console.log(duration)
+    localStorage.setItem('favorites', JSON.stringify(favorites))
 
+    //BASIC STYLE AND JSX 
     const style = {
-        backgroundImage: `linear-gradient(to top, #dfdfdf, rgba(233,233,233,.75), rgba(233,233,233,.95)),url(${anime?.data.data.images.webp.image_url})`
+        backgroundImage: `linear-gradient(to top, #dfdfdf, rgba(233,233,233,.75), rgba(233,233,233,.95)),url(${anime?.images.webp.image_url})`
     }
 
     return (
         <section className='anime-page'>
             <div className="anime-page-main">
                 <div className="anime-page-heading"  style={style}>
-                    <img loading='lazy' className='anime-page-image shadow' alt={anime?.data.data.title_english || anime?.data.data.titles[1] || anime?.data.data.title_japanese } src={anime?.data.data.images.webp.image_url} />
-                    {url[0] && <a href={url[0].url} rel="noreferrer" target="_blank" style={{padding:'.5rem 1rem'}} className="featured-watch shadow">WATCH NOW <ion-icon name="play-sharp"></ion-icon></a>}
+                    <img loading='lazy' className='anime-page-image shadow' alt={anime?.title_english || anime?.titles[1] || anime?.title_japanese } src={anime?.images.webp.image_url}/>
+                    <div style={{display:'flex', gap:'1rem', width:'100%',justifyContent:'space-between'}}>
+                    {url[0] && <a href={url[0].url} rel="noreferrer" target="_blank" style={{padding:'.5rem 1rem', width:'100%', fontWeight:'800'}} className="featured-watch shadow">WATCH NOW <ion-icon name="play-sharp"></ion-icon></a>}
+                    <button className='anime-favorite shadow' onClick={(e) => handleFavorite(e, anime?.mal_id)}>{!isFavorite ? 'Like' : 'Dislike'}<ion-icon className='shadow' name={!isFavorite ? 'heart-sharp' : 'heart-dislike-sharp'}></ion-icon></button>
+
+                    </div>
                 </div>
                 <div className="anime-page-test">
-                    <h2>{anime?.data.data.title_english || anime?.data.data.titles[0].title || anime?.data.data.title_japanese || 'Unknown'}</h2>
+                    <h2>{anime?.title_english || anime?.titles[0].title || anime?.title_japanese || 'Unknown'}</h2>
                     <div className="anime-page-info">
-                        {<p className='shadow rating'><ion-icon name="film-sharp"></ion-icon> {rating || 'unkown'}</p>}
-                        {<p className='shadow score'><ion-icon name="star"></ion-icon> {score || 'unkown'}</p>}
-                        {<p className='shadow duration'><ion-icon name="time-sharp"></ion-icon> {duration === 'Unknown' ? ' ? min' : `${duration === '1' || duration === '2' ? `${duration}hr` : `${duration}min`}`}</p>}
+                        <p className='shadow rating'><ion-icon name="film-sharp"></ion-icon> {rating || 'unkown'}</p>
+                        <p className='shadow score'><ion-icon name="star"></ion-icon> {score || 'unkown'}</p>
+                        <p className='shadow duration'><ion-icon name="time-sharp"></ion-icon>  {duration === 'Unknown' ? ' ? min' : `${duration === '1' || duration === '2' ? `${duration}hr` : `${duration}min`}`}</p>
                     </div>
-                    <p className='anime-page-desc'>{anime?.data.data.synopsis || 'There is no description for this anime.'}</p>
+                    <p className='anime-page-desc'>{anime?.synopsis || 'There is no description for this anime.'}</p>
                 </div>
             </div>
             <div className="anime-page-recommended">
                { 
-                <><h3 style={{display:'flex', alignItems:'center', gap:'.5rem'}}>Similar to this <ion-icon name="play-forward-sharp"></ion-icon></h3>
-                <div className="recommended-animes">
-                {recommended?.data.data ? recommended?.data.data.map(recommendedAnime => <Anime id={recommendedAnime.entry.mal_id} key={recommendedAnime.entry.mal_id} title={recommendedAnime.entry.title || recommendedAnime.entry.title_japanese || 'Unknown'} image={recommendedAnime.entry.images.webp.image_url}/>) : <p>We could not find any similar Anime to this...</p>}
-                </div></>
+                <>
+                    <h3 style={{display:'flex', alignItems:'center', gap:'.5rem'}}>Similar to this <ion-icon name="play-forward-sharp"></ion-icon></h3>
+                    <div className="recommended-animes">
+                    {recommended?.data.data ? recommended?.data.data.map(recommendedAnime => <Anime id={recommendedAnime.entry.mal_id} key={recommendedAnime.entry.mal_id} title={recommendedAnime.entry.title || recommendedAnime.entry.title_japanese || 'Unknown'} image={recommendedAnime.entry.images.webp.image_url}/>) : <p>We could not find any similar Anime to this...</p>}
+                    </div>
+                </>
                }
             </div>
         </section>
