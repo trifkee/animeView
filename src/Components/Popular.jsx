a2f62f6c4562dc45a3da1783dafe50da364d34ac
Trifunović Lazar <80207443+trifkee@users.noreply.github.com>
2023-03-08 01:25:54 +0100

    Update Popular.jsx

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -5,15 +5,14 @@ function Popular({title, genreList}) {
 
   return (
     <>
-    <section className='popular-section'>
-      
-            <h2>{title}</h2>
-            <div className="popular-catalog">
-                {genreList?.data.data.map(anime => <Anime key={anime.mal_id} id={anime.mal_id} image={anime.images.jpg.image_url} title={anime.title_english || anime.title_japanese || 'unknown' } year={anime.year} props={anime}/>)}
-            </div>
-            
-    </section>
-    
+      <section className='popular-section'>
+              
+              <h2>{title}</h2>
+              <div className="popular-catalog">
+                  {genreList?.data.data.map(anime => <Anime key={anime.mal_id} id={anime.mal_id} image={anime.images.jpg.image_url} title={anime.title_english || anime.title_japanese || 'unknown' } year={anime.year} props={anime}/>)}
+              </div>
+              
+      </section>
     </>
   )
 }
